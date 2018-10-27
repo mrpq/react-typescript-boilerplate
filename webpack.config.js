876182b355b60750ca72947f3084184c652379b8
Victor Woronin <gescbnet@yandex-team.ru>
2018-10-27 19:33:40 +0300

    обновит зависимости

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,70 @@
-const merge = require("webpack-merge");
-const path = require("path");
+// tslint:disable
+const merge = require('webpack-merge');
+const path = require('path');
 
-const parts = require("./webpack.parts.js");
+const parts = require('./webpack.parts.js');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 const PATHS = {
-  root: path.resolve(__dirname),
-  app: path.resolve(path.join(__dirname, "src"))
+    root: path.resolve(__dirname),
+    app: path.resolve(path.join(__dirname, 'src')),
 };
 
 const commonConfig = merge([
-  {
-    entry: path.join(PATHS.app, "index.js"),
-    output: {
-      filename: "index.js",
-      path: path.join(PATHS.root, "./dist")
-    }
-  },
-  {
-    plugins: [
-      new HtmlWebpackPlugin({
-        // Required
-        inject: false,
-        template: require("html-webpack-template"),
-        // Optional - https://github.com/jaketrent/html-webpack-template
-        // много дополнительных полей для темплейта генерируемого index.html
-        appMountId: "root"
-      }),
-      new ErrorOverlayPlugin()
-    ]
-  },
-  {
-    resolve: {
-      extensions: ["*", ".js", ".json", ".jsx"]
-    }
-  }
+    {
+        entry: path.join(PATHS.app, 'index.js'),
+        output: {
+            filename: 'index.js',
+            path: path.join(PATHS.root, './dist'),
+        },
+    },
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                // Required
+                inject: false,
+                template: require('html-webpack-template'),
+                // Optional - https://github.com/jaketrent/html-webpack-template
+                // много дополнительных полей для темплейта генерируемого index.html
+                appMountId: 'root',
+            }),
+            new ErrorOverlayPlugin(),
+        ],
+    },
+    {
+        resolve: {
+            extensions: ['*', '.js', '.json', '.jsx'],
+        },
+    },
 ]);
 
 const productionConfig = merge([
-  parts.loadJS({ exclude: [/node_modules/] }),
-  parts.loadCSS(),
-  parts.generateSourceMaps({ type: "source-map" }),
-  parts.loadImg({
-    options: { limit: 15, name: "./static/img/[name].[hash:4].[ext]" }
-  })
+    parts.loadJS({ exclude: [/node_modules/] }),
+    parts.loadCSS(),
+    parts.generateSourceMaps({ type: 'source-map' }),
+    parts.loadImg({
+        options: { limit: 15, name: './static/img/[name].[hash:4].[ext]' },
+    }),
 ]);
 
 const developmentConfig = merge([
-  parts.loadJS(),
-  parts.devServer({ host: process.env.HOST, port: process.env.PORT }),
-  parts.loadCSS(),
-  parts.generateSourceMaps({ type: "inline-source-map" }),
-  parts.loadImg()
+    parts.loadJS(),
+    parts.devServer({ host: process.env.HOST, port: process.env.PORT }),
+    parts.loadCSS(),
+    parts.generateSourceMaps({ type: 'inline-source-map' }),
+    parts.loadImg(),
 ]);
 
 module.exports = (env, argv) => {
-  process.env.BABEL_ENV = env;
+    process.env.BABEL_ENV = env;
 
-  if (env === "production") {
-    return merge([commonConfig, productionConfig, { mode: env }]);
-  }
+    if (env === 'production') {
+        return merge([commonConfig, productionConfig, { mode: env }]);
+    }
 
-  if (env === "development") {
-    return merge([commonConfig, developmentConfig, { mode: env }]);
-  }
+    if (env === 'development') {
+        return merge([commonConfig, developmentConfig, { mode: env }]);
+    }
 };
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,124 +1,122 @@
 exports.devServer = ({ host, port } = {}) => ({
-  devServer: {
-    stats: "errors-only",
-    host,
-    port,
-    open: true,
-    overlay: true
-  }
+    devServer: {
+        stats: 'errors-only',
+        host,
+        port,
+        open: true,
+        overlay: true,
+    },
 });
 
 exports.generateSourceMaps = ({ type }) => ({
-  devtool: type
+    devtool: type,
 });
 
 exports.loadJS = ({ include, exclude } = {}) => ({
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include,
-        exclude,
-        use: ["babel-loader"]
-      }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include,
+                exclude,
+                use: ['babel-loader'],
+            },
+        ],
+    },
 });
 
 const autoprefix = () => ({
-  loader: "postcss-loader",
-  options: {
-    plugins: [require("autoprefixer")]
-  }
+    loader: 'postcss-loader',
+    options: {
+        plugins: [require('autoprefixer')],
+    },
 });
 
 exports.autoprefix = autoprefix;
 
 exports.loadCSS = ({ include, exclude } = {}) => {
-  const loaders = (options = { "css-loader": { modules: false } }) => [
-    { loader: "style-loader" },
-    {
-      loader: "css-loader",
-      options: { sourceMap: true, ...options["css-loader"] }
-    }
-  ];
+    const loaders = (options = { 'css-loader': { modules: false } }) => [
+        { loader: 'style-loader' },
+        {
+            loader: 'css-loader',
+            options: { sourceMap: true, ...options['css-loader'] },
+        },
+    ];
 
-  return {
-    module: {
-      rules: [
-        // regular css
-        {
-          test: /\.css$/,
-          include,
-          exclude: exclude ? [/\.module\./].concat(exclude) : /\.module\./,
-          use: loaders()
+    return {
+        module: {
+            rules: [
+                // regular css
+                {
+                    test: /\.css$/,
+                    include,
+                    exclude: exclude ? [/\.module\./].concat(exclude) : /\.module\./,
+                    use: loaders(),
+                },
+                // regular sass
+                {
+                    test: /\.s(a|c)ss$/,
+                    include,
+                    exclude: exclude ? [/\.module\./].concat(exclude) : /\.module\./,
+                    use: loaders().concat({
+                        loader: 'sass-loader',
+                        options: { sourceMap: true },
+                    }),
+                },
+                // regular stylus
+                {
+                    test: /\.styl$/,
+                    include,
+                    exclude: exclude ? [/\.module\./].concat(exclude) : /\.module\./,
+                    use: loaders().concat({
+                        loader: 'stylus-loader',
+                        options: { sourceMap: true },
+                    }),
+                },
+                // css-modules css
+                {
+                    test: /\.module\.css$/,
+                    include,
+                    exclude,
+                    use: loaders({ 'css-loader': { modules: true } }),
+                },
+                // css-modules sass
+                {
+                    test: /\.module\.s(a|c)ss$/,
+                    include,
+                    exclude,
+                    use: loaders({ 'css-loader': { modules: true } }).concat({
+                        loader: 'sass-loader',
+                        options: { sourceMap: true },
+                    }),
+                },
+                // css-modules stylus
+                {
+                    test: /\.module\.styl$/,
+                    include,
+                    exclude,
+                    use: loaders({ 'css-loader': { modules: true } }).concat({
+                        loader: 'stylus-loader',
+                        options: { sourceMap: true },
+                    }),
+                },
+            ],
         },
-        // regular sass
-        {
-          test: /\.s(a|c)ss$/,
-          include,
-          exclude: exclude ? [/\.module\./].concat(exclude) : /\.module\./,
-          use: loaders().concat({
-            loader: "sass-loader",
-            options: { sourceMap: true }
-          })
-        },
-        // regular stylus
-        {
-          test: /\.styl$/,
-          include,
-          exclude: exclude ? [/\.module\./].concat(exclude) : /\.module\./,
-          use: loaders().concat({
-            loader: "stylus-loader",
-            options: { sourceMap: true }
-          })
-        },
-        // css-modules css
-        {
-          test: /\.module\.css$/,
-          include,
-          exclude,
-          use: loaders({ "css-loader": { modules: true } })
-        },
-        // css-modules sass
-        {
-          test: /\.module\.s(a|c)ss$/,
-          include,
-          exclude,
-          use: loaders({ "css-loader": { modules: true } }).concat({
-            loader: "sass-loader",
-            options: { sourceMap: true }
-          })
-        },
-        // css-modules stylus
-        {
-          test: /\.module\.styl$/,
-          include,
-          exclude,
-          use: loaders({ "css-loader": { modules: true } }).concat({
-            loader: "stylus-loader",
-            options: { sourceMap: true }
-          })
-        }
-      ]
-    }
-  };
+    };
 };
 
-exports.loadImg = ({ include, exclude, options } = {}) => {
-  return {
+exports.loadImg = ({ include, exclude, options } = {}) => ({
     module: {
-      rules: [
-        {
-          test: /\.(jpg|png|gif|webp)$/,
-          include,
-          exclude,
-          use: {
-            loader: "url-loader",
-            options
-          }
-        }
-      ]
-    }
-  };
-};
+        rules: [
+            {
+                test: /\.(jpg|png|gif|webp)$/,
+                include,
+                exclude,
+                use: {
+                    loader: 'url-loader',
+                    options,
+                },
+            },
+        ],
+    },
+});
